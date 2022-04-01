ae224fc2bc1542069bb38e4695df8d6544b227d4
usuario <alu0101333588@ull.edu.es>
2022-04-01 20:36:18 +0000

    actualizacion

--- Practica/Block.h
+++ Practica/Block.h
@@ -4,23 +4,23 @@
 #include "Sequence.h"
 
 template<class Key>
-class Sequence {
+class Block: public Sequence {
     public:
-    Sequence() {}
-    virtual ~Sequence() = default;
+    Block() {}
+    virtual ~Block() = default;
     
-    virtual bool Search(const Key& k) const = 0;
-    virtual bool Insert(const Key& k) = 0;
-    virtual bool IsFull() const = 0;
+    bool Search(const Key& k) const;
+    bool Insert(const Key& k);
+    bool IsFull() const;
     
     //int get_max_size() const;
     //void set_max_size(const int& max_size);
     //std::vector<Key> get_vector();
     
-    private:
-    std::list<> table;
+    //private:
+    //std::list<> table;
     //list<std::vector<double>>
     //std::vector<Key> table;
-    int tableSize;
-    int blockSize;
+    //int tableSize;
+    //int blockSize;
 };
--- Practica/Sequence.h
+++ Practica/Sequence.h
@@ -4,13 +4,17 @@
 
 template<class Key>
 class Sequence {
- public:
-  Sequence() {}
-  virtual ~Sequence() = default;
+  public:
+    Sequence() {}
+    virtual ~Sequence() = default;
 
-  virtual bool Search(const Key& k) const = 0;
-  virtual bool Insert(const Key& k) = 0;
-  virtual bool IsFull() const = 0;
+    virtual bool Search(const Key& k) const = 0;
+    virtual bool Insert(const Key& k) = 0;
+    virtual bool IsFull() const = 0;
+  
+  protected:
+    std::list<Key> table_;
+    int blockSize_;
 
   //int get_max_size() const;
   //void set_max_size(const int& max_size);
